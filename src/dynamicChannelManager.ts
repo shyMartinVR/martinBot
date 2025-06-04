852ffa08cf5b8a838f57927eaf12483568a14b81
shyMartin <martin@shymartin.dev>
2025-06-04 18:43:45 +0200

    basic error handling

--- src/dynamicChannelManager.ts
+++ src/dynamicChannelManager.ts
@@ -1,4 +1,4 @@
-import {  CategoryChannel, Client, Collection, Events, Interaction, Snowflake, VoiceBasedChannel, VoiceState } from 'discord.js';
+import { CategoryChannel, Client, Collection, Events, Interaction, Snowflake, VoiceBasedChannel, VoiceState } from 'discord.js';
 import DynamicChannel, { DynamicChannelInteraction } from './dynamicChannel';
 import ChannelDatabase from './database';
 
@@ -22,100 +22,112 @@ export default class DynamicChannelManager {
   }
 
   private async loadFromDatabase() {
-    const rows = this.database.getChannels();
-    for (const { channelId, ownerId } of rows) {
-      const channel = await this.client.channels.fetch(channelId);
-      if (!channel || !channel.isVoiceBased()) {
-        console.warn('Channel not found or not a voice channel:', channelId);
-        this.database.removeChannel(channelId);
-        continue;
-      }
+    try {
+      const rows = this.database.getChannels();
+      for (const { channelId, ownerId } of rows) {
+        const channel = await this.client.channels.fetch(channelId);
+        if (!channel || !channel.isVoiceBased()) {
+          console.warn('Channel not found or not a voice channel:', channelId);
+          this.database.removeChannel(channelId);
+          continue;
+        }
 
-      if (channel.members.size === 0) {
-        console.warn('Channel is empty:', channelId);
-        this.database.removeChannel(channelId);
-        channel.delete();
-        continue;
-      }
+        if (channel.members.size === 0) {
+          console.warn('Channel is empty:', channelId);
+          this.database.removeChannel(channelId);
+          channel.delete();
+          continue;
+        }
 
-      let owner = channel.members.get(ownerId);
-      if (!owner) {
-        console.warn('Owner member not found in channel:', ownerId, 'for channel', channelId);
-        owner = channel.members.random(); 
-        this.database.setChannel(channel, owner);
+        let owner = channel.members.get(ownerId);
+        if (!owner) {
+          console.warn('Owner member not found in channel:', ownerId, 'for channel', channelId);
+          owner = channel.members.random();
+          this.database.setChannel(channel, owner);
+        }
+
+        const dynamicChannel = new DynamicChannel(channel, owner);
+        this.channels.set(channelId, dynamicChannel);
+        console.info('Loaded dynamic channel:', dynamicChannel.channel.name, 'for owner:', owner.displayName);
       }
-      
-      const dynamicChannel = new DynamicChannel(channel, owner);
-      this.channels.set(channelId, dynamicChannel);
-      console.info('Loaded dynamic channel:', dynamicChannel.channel.name, 'for owner:', owner.displayName);
+    } catch (error) {
+      console.error(error);
     }
   }
 
   private onInteractionCreate(interaction: Interaction) {
-    if(!interaction.channelId || !this.channels.has(interaction.channelId)) {
-      console.warn('No dynamic channel found for interaction in channel', interaction.channelId);
-      return;
-    }
-
-    const channel = this.channels.get(interaction.channelId)!;
-
-    if (interaction.isButton()) {
-      switch (interaction.customId) {
-        case DynamicChannelInteraction.RenameButton:
-          channel.handleRenameButton(interaction);
-          break;
-        case DynamicChannelInteraction.GuestInviteButton:
-          channel.handleGuestInviteButton(interaction);
-          break;
-        default:
-          console.warn('Unknown button interaction:', interaction.customId, 'in channel', interaction.channelId);
-          break;
-      } 
-    } else if (interaction.isModalSubmit()) {
-      if(interaction.customId === DynamicChannelInteraction.RenameModal) {
-        channel.handleRenameModal(interaction);
+    try {
+      if (!interaction.channelId || !this.channels.has(interaction.channelId)) {
+        console.warn('No dynamic channel found for interaction in channel', interaction.channelId);
+        return;
       }
+
+      const channel = this.channels.get(interaction.channelId)!;
+
+      if (interaction.isButton()) {
+        switch (interaction.customId) {
+          case DynamicChannelInteraction.RenameButton:
+            channel.handleRenameButton(interaction);
+            break;
+          case DynamicChannelInteraction.GuestInviteButton:
+            channel.handleGuestInviteButton(interaction);
+            break;
+          default:
+            console.warn('Unknown button interaction:', interaction.customId, 'in channel', interaction.channelId);
+            break;
+        }
+      } else if (interaction.isModalSubmit()) {
+        if (interaction.customId === DynamicChannelInteraction.RenameModal) {
+          channel.handleRenameModal(interaction);
+        }
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 
   private async onVoiceStateUpdate(oldState: VoiceState, newState: VoiceState) {
-    // No change in voice channel
-    if (newState.channelId === oldState.channelId) {
-      return;
-    }
-
-    // User left a dynamic channel
-    if (oldState.member && oldState.channel?.id && this.channels.has(oldState.channel.id)) {
-      console.info(oldState.member.displayName, 'left', oldState.channel?.name);
-
-      const dynamicChannel = this.channels.get(oldState.channel.id)!;
-      dynamicChannel.removeMember(oldState.member, this.database);
-      console.assert(dynamicChannel.members.size === oldState.channel.members.size, `Member count mismatch in channel ${oldState.channel.name}. Expected: ${dynamicChannel.members.size}, Actual: ${oldState.channel.members.size}`);
-      if (dynamicChannel.isEmpty()) {
-        console.info('Deleting', dynamicChannel.channel.name);
-        dynamicChannel.channel.delete();
-        this.channels.delete(dynamicChannel.channel.id);
-        this.database.removeChannel(dynamicChannel.channel);
+    try {
+      // No change in voice channel
+      if (newState.channelId === oldState.channelId) {
+        return;
       }
-    }
 
-    // User joined setup channel
-    if (newState.member && newState.channel?.id === this.setup.id) {
-      console.info(newState.member.displayName, 'joined setup channel');
+      // User left a dynamic channel
+      if (oldState.member && oldState.channel?.id && this.channels.has(oldState.channel.id)) {
+        console.info(oldState.member.displayName, 'left', oldState.channel?.name);
 
-      const newChannel = await DynamicChannel.create(newState.member, this.parent, this.setup.position + 1)
-      this.channels.set(newChannel.channel.id, newChannel);
-      newState.member.voice.setChannel(newChannel.channel);
-      this.database.setChannel(newChannel.channel, newState.member);
-    }
-    
-    // User joined an existing dynamic channel
-    if (newState.member && newState.channel?.id && this.channels.has(newState.channel.id)) {
-      console.info(newState.member.displayName, 'joined', newState.channel?.name);    
+        const dynamicChannel = this.channels.get(oldState.channel.id)!;
+        dynamicChannel.removeMember(oldState.member, this.database);
+        console.assert(dynamicChannel.members.size === oldState.channel.members.size, `Member count mismatch in channel ${oldState.channel.name}. Expected: ${dynamicChannel.members.size}, Actual: ${oldState.channel.members.size}`);
+        if (dynamicChannel.isEmpty()) {
+          console.info('Deleting', dynamicChannel.channel.name);
+          dynamicChannel.channel.delete();
+          this.channels.delete(dynamicChannel.channel.id);
+          this.database.removeChannel(dynamicChannel.channel);
+        }
+      }
 
-      const dynamicChannel = this.channels.get(newState.channel.id)!;
-      dynamicChannel.addMember(newState.member);
-      console.assert(dynamicChannel.members.size === newState.channel.members.size, `Member count mismatch in channel ${newState.channel.name}. Expected: ${dynamicChannel.members.size}, Actual: ${newState.channel.members.size}`);
+      // User joined setup channel
+      if (newState.member && newState.channel?.id === this.setup.id) {
+        console.info(newState.member.displayName, 'joined setup channel');
+
+        const newChannel = await DynamicChannel.create(newState.member, this.parent, this.setup.position + 1)
+        this.channels.set(newChannel.channel.id, newChannel);
+        newState.member.voice.setChannel(newChannel.channel);
+        this.database.setChannel(newChannel.channel, newState.member);
+      }
+
+      // User joined an existing dynamic channel
+      if (newState.member && newState.channel?.id && this.channels.has(newState.channel.id)) {
+        console.info(newState.member.displayName, 'joined', newState.channel?.name);
+
+        const dynamicChannel = this.channels.get(newState.channel.id)!;
+        dynamicChannel.addMember(newState.member);
+        console.assert(dynamicChannel.members.size === newState.channel.members.size, `Member count mismatch in channel ${newState.channel.name}. Expected: ${dynamicChannel.members.size}, Actual: ${newState.channel.members.size}`);
+      }
+    } catch (error) {
+      console.error(error);
     }
   }
 }
